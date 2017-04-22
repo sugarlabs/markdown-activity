91e9c575caf1c078738a0ba46382519d329bdbcc
Tony Anderson <tony37@schoolserver.lan>
2017-04-22 18:15:37 +0800

    v2

--- js/activity.js
+++ js/activity.js
@@ -13,10 +13,13 @@ define(function (require) {
         activity.setup();
 
         inputTextContent = document.getElementById("wmd-input-second");
-        inputTextContent.value = "#This is a sample input";
+
         //to save and resume the contents from datastore.
 
         var datastoreObject = activity.getDatastoreObject();
+
+        console.log(datastoreObject);
+        console.log(datastoreObject);
 
         inputTextContent.onblur = function () {
             console.log(inputTextContent.value);
@@ -24,21 +27,17 @@ define(function (require) {
             datastoreObject.setDataAsText(jsonData);
             datastoreObject.save(function () {});
         };
-        markdownParsing(); //to load for the first time
+
+        markdownParsing();
+
         datastoreObject.loadAsText(function (error, metadata, data) {
-            console.log(metadata);
-            console.log(data);
             markdowntext = JSON.parse(data);
-
-            console.log(markdowntext);
-
             inputTextContent.value = markdowntext;
-            markdownParsing(); //to load again when there is a datastore entry
+            markdownParsing(); //it has to parse only after the input text box has been loaded
         });
 
         function markdownParsing() {
             var converter2 = new Markdown.Converter();
-
             var help = function () {
                 alert("Do you need help?");
             }
@@ -50,12 +49,11 @@ define(function (require) {
                     quoteexample: "whatever you're quoting, put it right here"
                 }
             };
-
             var editor2 = new Markdown.Editor(converter2, "-second", options);
 
             editor2.run();
-        }
 
+        }
     });
 
 });
